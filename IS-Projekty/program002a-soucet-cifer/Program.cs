3b52bfc8b08e283a7dc1605dc0600e754566d6df
David Pavlů <david.pavlu1@tul.cz>
2024-10-17 13:23:25 +0200

    #2 součet cifer - DONE

---
+++ IS-Projekty/program002a-soucet-cifer/Program.cs
@@ -0,0 +1,48 @@
+﻿// CHCI, ABY SE PROGRAM OPAKOVAL PO STISKU KLÁVESY "A"
+        string again = "a";
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("*******************************");
+            Console.WriteLine("**** Součet a součin cifer ****");
+            Console.WriteLine("*******************************");
+            Console.WriteLine("********* David Pavlů *********");
+            Console.WriteLine("*******************************");
+            Console.WriteLine();
+
+            // Vstup od uživatel - lepší varianta
+            Console.WriteLine("Zadejte celé číslo jehož ciferný součet chcete spočítat: ");
+            int number;
+            while(!int.TryParse(Console.ReadLine(), out number)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte celé číslo: ");
+            }
+
+            // výpis vtupní hodnoty
+            Console.WriteLine("===========================");
+            Console.WriteLine("Uživatel zadal: {0}", number);
+            Console.WriteLine("===========================\n\n");
+
+
+            int suma = 0;
+            int numberbackup = number;
+            int digit;
+
+            // Pokud je číslo záporn, tak mínus odstraníme
+            if (number <0) {
+                number = - number;
+            }
+
+            while(number >= 10) {
+                digit = number % 10;  // % operátor modulo
+                number = (number - digit) / 10;
+                Console.WriteLine("Digit = {0}", digit);
+                suma = suma + digit;
+            }
+            Console.WriteLine("Digit = {0}", number);
+            suma = suma + number;
+
+            Console.WriteLine("\n\nSoučet cifer čísla {0} je {1}", numberbackup, suma);
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
